994032cf815fb5192cdd8cc21462a4b580de0fd7
Anthony Animba <anthonyc.animba@gmail.com>
2022-05-30 00:23:05 +0100

    --

--- components/styles/Features/Features.ts
+++ components/styles/Features/Features.ts
@@ -2,15 +2,6 @@ import styled from "styled-components";
 
 export const Features = styled.div`
   margin-top: 150px;
-  h2 {
-    font-weight: 500;
-    font-size: 2.5vw;
-    line-height: 1.35;
-
-    @media screen and (min-width: 1600px) {
-      font-size: 2vw;
-    }
-  }
 
   p {
     margin-top: 20px;
